5ecc5b518f2ee94652d885bb66f4ef7256e0b945
francisco eduardo gonzalez <90358593+franciscogonzalezf20@users.noreply.github.com>
2023-01-25 15:06:14 -0600

    se hace el validador
    se crea una instancia de la clase Main, que contiene una instancia de ValidadorFactory y un método validar_numero(). Este método recibe un número y una cantidad de dígitos esperados. Utiliza la instancia de ValidadorFactory para crear una instancia de Validador13Digitos o Validador8Digitos dependiendo de la cantidad de dígitos esperada. Luego llama al método validar() en la instancia de Validador creada y determina si el número es válido o no.
    
    En este ejemplo, se llama al método validar_numero() con 3 números diferentes, uno con 13 dígitos, otro c

--- impl-hu-webscraping-be-Develop/sat_prueba_concepto/spider_sat_prueba/Validador/Validador.py
+++ impl-hu-webscraping-be-Develop/sat_prueba_concepto/spider_sat_prueba/Validador/Validador.py
@@ -1,9 +1,45 @@
 from abc import abstractmethod
 from abc import ABC
-
+"""
 
 class Validador(ABC):
 
     @abstractmethod
     def validar(self):
         pass
+"""
+
+class Validador13Digitos(Validador):
+    def validar(self, number: str):
+        if len(number) == 13:
+            return True
+        return False
+
+class Validador8Digitos(Validador):
+    def validar(self, number: str):
+        if len(number) == 8:
+            return True
+        return False
+
+class ValidadorFactory:
+    def create_validator(self, digitos: int):
+        if digitos == 13:
+            return Validador13Digitos()
+        elif digitos == 8:
+            return Validador8Digitos()
+
+class Main:
+    def __init__(self):
+        self.validador_factory = ValidadorFactory()
+
+    def validar_numero(self, number: str, digitos: int):
+        validador = self.validador_factory.create_validator(digitos)
+        if validador.validar(number):
+            print("El número es válido.")
+        else:
+            print("El número no es válido.")
+
+main = Main()
+main.validar_numero("1234567890123", 13) # El número es válido.
+main.validar_numero("12345678", 8) # El número es válido.
+main.validar_numero("123456789", 13) # El número no es válido.
